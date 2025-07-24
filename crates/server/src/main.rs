f2ebff7b76aa8cc8501377f56d51a8f5f0a80b98
Olivia Brode-Roger <nibrivia@gmail.com>
2025-07-24 12:15:43 -0400

    stopping point

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,15 +1,40 @@
+use chatbot::{gen_random_number, query_chat};
 use miniserve::{Content, Request, Response};
-use tokio::join;
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::join;
 
 async fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Messages {
     messages: Vec<String>,
+}
+
+async fn make_response(input_json: String) -> Option<String> {
+    // Parse
+    let mut messages: Messages = serde_json::from_str(&input_json).ok()?;
+
+    // Call async
+    let response_index_fut = tokio::spawn(gen_random_number());
+
+    let mut messages_arc = Arc::new(messages);
+    let possible_responses_fut = tokio::spawn(query_chat(&messages_arc.messages));
+
+    let (ix_res, responses_res) = join!(response_index_fut, possible_responses_fut);
+
+    // Build response
+    let mut possible_responses = responses_res.ok()?;
+    let response_msg = possible_responses.remove(ix_res.ok()? % possible_responses.len());
+    Arc::<Messages>::get_mut(&mut messages_arc)?
+        .messages
+        .push(response_msg);
+    let response_json = serde_json::to_string(&(Arc::into_inner(messages_arc).unwrap())).ok()?;
+
+    return Some(response_json);
 }
 
 async fn chat(_req: Request) -> Response {
@@ -17,20 +42,9 @@ async fn chat(_req: Request) -> Response {
         // TODO: Use actual HTTP error codes here instead?
         Request::Get => Ok(Content::Json("Malformed request".to_string())),
         Request::Post(umsgs) => {
-            let mut messages: Messages = serde_json::from_str(&umsgs).unwrap_or(Messages {
-                messages: vec!["parse messages fail".to_string()],
-            });
-
-            let response_index_fut = chatbot::gen_random_number();
-            let possible_responses_fut = chatbot::query_chat(&messages.messages);
-            let (ix, mut possible_responses) = join!(response_index_fut, possible_responses_fut);
-
-
-            let response_msg = possible_responses.remove(ix % possible_responses.len());
-
-            messages.messages.push(response_msg);
-
-            let response = serde_json::to_string(&messages).unwrap_or("to_string fail".to_string());
+            let response = make_response(umsgs)
+                .await
+                .unwrap_or("response error".to_string());
             Ok(Content::Json(response))
         }
     }
